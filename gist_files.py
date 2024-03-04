fd17bf8429e29c2d916c8b21ac4ff7b55b57efea
jyouturner <jy2947@gmail.com>
2024-03-04 17:01:53 -0500

    add check

--- gist_files.py
+++ gist_files.py
@@ -51,7 +51,11 @@ if __name__ == "__main__":
         sys.exit(1)
     pf = ProjectFiles(root_path=root_path, prefix_list=["src/main/java"], suffix_list=[".java"])
     # scan the files in the project, and create package structure
-
+    pf.from_gist_files()
+    if pf.files is not None:
+        print(f"Gist files already exist at {root_path}, are you sure you want to continue? (y/n)")
+        if input() != "y":
+            sys.exit(1)
     # now talk with LLM to get the notes of files
     codefiles = pf.get_files_of_project()
     input("Press Enter to continue to send request to LLM ...")
@@ -63,3 +67,4 @@ if __name__ == "__main__":
     # now that we have the notes for each file, we can persist them to a file for future use
     gist_file_path = pf.persist_code_files(codefiles)
     print(f"Gist file is persisted to {gist_file_path}")
+    print("Please run the gist_package.py to recreate the package notes.")
