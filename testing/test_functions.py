f64864838c54f0d7891ddc810796cddc291275ca
jyouturner <JUNQIANG_YOU@homedepot.com>
2025-03-01 22:48:41 -0500

    Merge branch 'main' into v2-knowledge-graph-augmented-RAG
    File: testing/test_functions.py

--- testing/test_functions.py
+++ testing/test_functions.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from projectfiles import ProjectFiles
+from gist.projectfiles import ProjectFiles
 import os
 from functions import get_file, get_package, read_files
 
