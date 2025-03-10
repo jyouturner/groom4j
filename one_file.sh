25af5412d47d818952aca137a07eb81b24c5305a
jyouturner <JUNQIANG_YOU@homedepot.com>
2025-03-10 00:35:16 -0400

    add script
    File: one_file.sh

---
+++ one_file.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+./combine_files.sh -e "*.sh" -e testing -e tests -e data -e gist -e integration -e MagicMock -e observability -e output -e __pycache__ -e docs -e .pytest_cache -e "*.md"
